4168bbf43b92fbef921a8dab6459860b1c0f2ad6
iliana <iliana.decarufel@gmail.com>
2024-10-30 12:16:56 -0400

    Adding a controller interface to publish odrive data

---
+++ odrive_joint_broadcaster/include/odrive_joint_broadcaster/odrive_joint_broadcaster.hpp
@@ -0,0 +1,139 @@
+// Copyright (c) 2024, capra
+// Copyright (c) 2024, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+// Source of this file are templates in
+// [RosTeamWorkspace](https://github.com/StoglRobotics/ros_team_workspace) repository.
+//
+
+#ifndef ODRIVE_JOINT_BROADCASTER__ODRIVE_JOINT_BROADCASTER_HPP_
+#define ODRIVE_JOINT_BROADCASTER__ODRIVE_JOINT_BROADCASTER_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "controller_interface/controller_interface.hpp"
+#include "odrive_joint_broadcaster/visibility_control.h"
+// #include "odrive_joint_broadcaster_parameters.hpp"
+#include "../../../build/odrive_joint_broadcaster/odrive_joint_broadcaster_parameters/include/odrive_joint_broadcaster_parameters.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "realtime_tools/realtime_buffer.h"
+#include "realtime_tools/realtime_publisher.h"
+
+// Messages and services
+#include "odrive_joint_broadcaster/msg/o_drive_joint_state.hpp"
+#include "odrive_joint_broadcaster/srv/request_clear_errors.hpp"
+#include "odrive_joint_broadcaster/srv/request_reboot.hpp"
+
+namespace odrive_joint_broadcaster
+{
+// name constants for state interfaces
+enum class joint_state_itfs : std::uint8_t
+{
+  BUS_VOLTAGE = 0,
+  BUS_CURRENT,
+  FET_TEMPERATURE,
+  MOTOR_TEMPERATURE,
+  AXIS_STATE,
+  ACTIVE_ERRORS,
+  DISARM_REASON,
+  COUNT
+};
+
+enum class joint_command_itfs : std::uint8_t
+{
+  REQUEST_CLEAR_ERRORS = 0,
+  REQUEST_REBOOT,
+  COUNT
+};
+
+const std::vector<std::string> JointStateITFS = {
+  "bus_voltage",
+  "bus_current",
+  "fet_temperature",
+  "motor_temperature",
+  "axis_state",
+  "active_errors",
+  "disarm_reason",
+};
+
+const std::vector<std::string> StateITFS = {
+  "average_voltage",
+  "total_current",
+};
+
+const std::vector<std::string> JointCommandITFS = {
+  "request_clear_errors",
+  "request_reboot",
+};
+
+class ODriveJointBroadcaster : public controller_interface::ControllerInterface
+{
+public:
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  ODriveJointBroadcaster();
+
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_init() override;
+
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  controller_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_PUBLIC
+  controller_interface::return_type update(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+protected:
+  std::shared_ptr<odrive_joint_broadcaster::ParamListener> param_listener_;
+  odrive_joint_broadcaster::Params params_;
+
+  std::vector<std::string> joints_;
+
+  // Command subscribers and Controller State publisher
+
+  rclcpp::Service<RequestClearErrors>::SharedPtr request_clear_errors_service_;
+  realtime_tools::RealtimeBuffer<> control_mode_;
+
+  using ControllerStatePublisher = realtime_tools::RealtimePublisher<ControllerStateMsg>;
+
+  rclcpp::Publisher<ControllerStateMsg>::SharedPtr s_publisher_;
+  std::unique_ptr<ControllerStatePublisher> state_publisher_;
+
+private:
+  // callback for topic interface
+  ODRIVE_JOINT_BROADCASTER__VISIBILITY_LOCAL
+  void reference_callback(const std::shared_ptr<ControllerReferenceMsg> msg);
+};
+
+}  // namespace odrive_joint_broadcaster
+
+#endif  // ODRIVE_JOINT_BROADCASTER__ODRIVE_JOINT_BROADCASTER_HPP_
--- odrive_ros2_control/src/odrive_controller_interface.cpp
+++ odrive_ros2_control/src/odrive_controller_interface.cpp
@@ -1,6 +1,7 @@
 #include "controller_interface/controller_interface.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 #include "odrive_ros2_control/msg/o_drive_joint_state.hpp"
 #include "odrive_ros2_control/srv/request_clear_errors.hpp"
 #include "odrive_ros2_control/srv/request_reboot.hpp"
@@ -14,25 +15,29 @@ public:
     controller_interface::CallbackReturn on_init() override
     {
         // Initialize the publisher
-        voltage_pub_ = get_node()->create_publisher<std_msgs::msg::Float64>("flipper_voltage", 1);
-        current_pub_ = get_node()->create_publisher<std_msgs::msg::Float64>("flipper_current", 1);
+        voltage_pub_ = get_node()->create_publisher<std_msgs::msg::Float64>("odrive_voltage", 1);
+        current_pub_ = get_node()->create_publisher<std_msgs::msg::Float64>("odrive_current", 1);
         avg_voltage_pub_ = get_node()->create_publisher<std_msgs::msg::Float64>("average_voltage", 1);
+        state_pub_ = get_node()->create_publisher<sensor_msgs::msg::JointState>("odrive_state", 1);
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
     controller_interface::InterfaceConfiguration command_interface_configuration() const override
     {
-        
     }
 
     controller_interface::return_type update_and_write_commands(const rclcpp::Time & time, const rclcpp::Duration & period)
     {
         // Access the state interfaces (e.g., voltage and current) from the hardware interface
+        double voltage, current, error, state;
         for (auto& v : state_interfaces_)
         {
+            if (v.get_interface_name() == "voltage") voltage = v.get_value();
+            if (v.get_interface_name() == "current") current = v.get_value();
+            if (v.get_interface_name() == "error") error = v.get_value();
+            if (v.get_interface_name() == "state") state = v.get_value();
+            
         }
-        double voltage = state_interfaces_[0].get_value();  // Assuming state_interfaces_[0] is for voltage
-        double current = state_interfaces_[1].get_value();  // Assuming state_interfaces_[1] is for current
 
         // Publish voltage
         std_msgs::msg::Float64 voltage_msg;
@@ -48,6 +53,8 @@ public:
     }
 
 private:
+    sensor_msgs::msg::JointState states_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr state_pub_;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr avg_voltage_pub_;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr total_current_pub_;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr voltage_pub_;
